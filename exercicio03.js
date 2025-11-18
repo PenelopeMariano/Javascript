5e53cb7bcbd4a03bb8ac52cb36a29eb9b88da8f4
PENELOPE MARIANO FRAGNAN GOMES <46561476808@imaccommand028.local>
2025-11-18 11:19:45 -0300

    Exercicio03

---
+++ exercicio03.js
@@ -0,0 +1,12 @@
+// Exercício 3: Ímpar ou par
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let number = window.prompt ("Numeros par ou impar")
+
+if (number % 2 == 0) {
+    console.log ("Ele é par")
+}
+
+else 
+    console.log ("Ele é impar")
