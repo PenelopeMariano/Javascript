f1e49337c8ced0b25b6ad2268d12082e80c02dbe
PENELOPE MARIANO FRAGNAN GOMES <46561476808@imaccommand028.local>
2025-11-18 12:00:19 -0300

    Exercicio04

---
+++ exercicio04.js
@@ -0,0 +1,26 @@
+// Exercício 4: Notas escolares
+// Faça um programa que receba 4 notas de um aluno, calcule e imprima a média aritmética das
+// notas e a mensagem de APROVADO para média superior ou igual a 7,0 RECUPERAÇÃO para
+// notas entre 5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
+
+let nota1 = number.parsetFloat (prompt("Digite a nota de português"))
+
+let nota2 = number.parsetFloat (prompt("Digite a nota de historia"))
+
+let nota3 = number.parsetFloat (prompt("Digite a nota de matematica"))
+
+let nota4 = number.parsetFloat (prompt("Digite a nota de geografia"))
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+if (media >= 7){ 
+    console.log("aprovado")
+}
+
+else if (media > 5 && media <7){ 
+    console.log ("recuperação")
+}
+
+else { 
+    console.log ("reprovado")
+}
